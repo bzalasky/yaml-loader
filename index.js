fe610538e2038ab4924c4ff0c9672fc52a95f70a
Benjamin Zalasky <benjamin.zalasky@lookout.com>
2017-08-16 11:53:55 -0400

    Add debug log

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ module.exports = function (source) {
   this.cacheable && this.cacheable();
   try {
     var res = yaml.safeLoad(source);
+    console.log(res);
     return JSON.stringify(res, undefined, '\t');
   }
   catch (err) {
+    console.log(err);
     this.emitError(err);
     return null;
   }
