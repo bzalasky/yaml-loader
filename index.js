7563fd39edea7c3294457a7ad7f3d56b1bdbf4bf
Benjamin Zalasky <benjamin.zalasky@lookout.com>
2017-08-16 11:58:23 -0400

    Remove debug log, lock js-yaml version

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ module.exports = function (source) {
   this.cacheable && this.cacheable();
   try {
     var res = yaml.safeLoad(source);
-    console.log(res);
     return JSON.stringify(res, undefined, '\t');
   }
   catch (err) {
-    console.log(err);
     this.emitError(err);
     return null;
   }
